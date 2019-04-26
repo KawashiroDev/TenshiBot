bfd1908621eacffac500de127eca90ffa3bf4354
99710 <cuttsharry123@gmail.com>
2019-04-26 15:43:34 +0100

    Code improvements
    Other bots are now ignored
    Server count is displayed in console
    Commands are now activated by mention, still need to figure out how to set that by bot id

--- Tenshi.py
+++ Tenshi.py
@@ -4,7 +4,6 @@
 #this version is still in early development and NOT ready to replace the main version
 
 #todo
-#ignore other bots
 #server count display and posting
 #dev id checking for some commands
 
@@ -14,6 +13,9 @@
 #Token
 token = ''
 
+#Discordbots.org API key
+dbo_api = ''
+
 #Variant
 bot_variant = 'slipstream'
 
@@ -21,16 +23,25 @@ bot_variant = 'slipstream'
 bot_version = '1.5'
 
 #prefix
-#i tried not hardcoding this, it didn't work so here we are
-tb_prefix = ('<@225323275163664384> ')
+#Debug account has user ID 571094749537239042
+#Normal account has user ID 252442396879486976
+#i tried not hardcoding this, it didn't work 
+tb_prefix = ('<@571094749537239042> ')
+#tb_prefix = ('<@' + str(bot.user.id) + '>')
 
+
+#Booting text
 print('Please wait warmly...')
 
 import discord
 import requests
 
-client = discord.Client()
+from discord.ext import commands
 
+
+#bot = commands.Bot(command_prefix= '<@' + str(bot.user.id) + '> ')
+bot = commands.Bot(command_prefix= 'tb_prefix')
+client = discord.Client()
 
 #Prefix
 #Tenshi's Prefix, this has to be below the imports
@@ -38,39 +49,42 @@ client = discord.Client()
 #tb_prefix = ('<@' + client.user.id + '> ')
 
 #bot will display this on startup when accepting commands
-@client.event
+#NTS: Try using bot where there would normally be client eg client.user.name > bot.user.name
+
+@bot.event
 async def on_ready():
     print(' ')
     print('TenshiBot startup complete ')
     print(' ')
-    print('System ID - ' + client.user.id)
-    print('System Name - ' + client.user.name)
-    print('System Ver - ' + bot_version)
+    print('User ID - ' + str(bot.user.id))
+    print('Username - ' + bot.user.name)
+    print('TenshiBot Ver - ' + bot_version)
     print('System Variant - ' + bot_variant)
     print(' ')
-
-    print('servercount - ' + str(len(client.servers)))
-
-
-
-#discard other bot commands
-#async def on_message():
-#    if mesage.author.bot:
-#        return
+    print('servercount - ' + str(len(bot.guilds)))
+    print(discord.version_info)
 
 
-#test command
+#other bot ignoring code 
 @client.event
 async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+    if message.author.bot:
+        return
 
-#checks if the message starts with the prefix and the command
-    if message.content.startswith(tb_prefix + 'helloworld'):
-#prints the message to the console
-        print(message.content)
-#sends the response
-        await client.send_message(message.channel, "bhava-agra")
+#test commands
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+@bot.command()
+async def foo(ctx, arg):
+    await ctx.send(arg)
 
 
 
 #this has to be at the end of the code
-client.run(token)
+#client.run(token)
+bot.run(token, bot=True, reconnect=True)
