b1d2f23f928dc3f8fb55e77118d4ca7995885669
99710 <cuttsharry123@gmail.com>
2019-04-27 14:13:20 +0100

    Image command testing
    image commands work, just need to embed them like before

--- Tenshi.py
+++ Tenshi.py
@@ -14,6 +14,22 @@ tkn = open("test/token.txt", "r")
 token = tkn.read()
 tkn.close()
 
+#booru URL, used for touhou images and safebooru command
+booru = 'safebooru.org'
+
+#booru rating
+#options are: safe, questionable, explicit
+#affects the safebooru command only
+boorurating = 'safe'
+
+#booru tag blacklist
+#results which have these tags won't be shown in the touhou commands
+#does not affect the safebooru command
+boorublacklist = '-underwear+-sideboob+-pov_feet+-underboob+-upskirt+-sexually_suggestive+-ass+-bikini'
+
+#append text to the start of booru url output
+#change this if the bot is sending malformed booru urls
+booruappend = 'http:'
 
 #Discordbots.org API key
 dbo_api = ''
@@ -37,14 +53,16 @@ import discord
 import requests
 import praw
 import lxml
+import random
 
 from discord.ext import commands
 from bs4 import BeautifulSoup
 
 
 #bot = commands.Bot(command_prefix= '<@' + str(bot.user.id) + '> ')
-
+#do not leave '' here
 bot = commands.Bot(command_prefix= tb_prefix)
+client = discord.client
 
 
 #Prefix
@@ -81,6 +99,35 @@ async def on_message(message):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+
+
+#now the fun part, getting these working...
+#this works, just need to add the embeds    
+@bot.command()
+async def tenshi(ctx):
+    char = 'hinanawi_tenshi'
+    r = requests.get('http://' + booru + '/index.php?page=dapi&s=post&q=index&tags=solo+' + boorublacklist + '+' + char)
+    if r.status_code == 200:
+            soup = BeautifulSoup(r.text, "lxml")
+            num = int(soup.find('posts')['count'])
+            maxpage = int(round(num/100))
+            page = random.randint(0, maxpage)
+            t = soup.find('posts')
+            p = t.find_all('post')
+            if num == 0:
+                msg = 'No posts found'
+            else:
+                if num < 100:
+                    pic = p[random.randint(0,num-1)]
+                elif page == maxpage:
+                    pic = p[random.randint(0,num%100 - 1)]
+                else:
+                    pic = p[random.randint(0,99)]
+                msg = pic['file_url']
+            await ctx.send(booruappend + msg)
+    else:
+            msg = 'An error has occured'
+            await ctx.send(msg)
 
 
 
