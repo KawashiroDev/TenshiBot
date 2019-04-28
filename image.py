f1991ab5bc849c7826384184bfac178b7069fa24
99710 <cuttsharry123@gmail.com>
2019-04-28 12:06:09 +0100

    Update image.py

--- image.py
+++ image.py
@@ -60,6 +60,90 @@ class ImageCog(commands.Cog):
                     em.set_author(name='Character Image')
                     em.set_image(url=booruappend + msg)
                     await ctx.send(embed=em)  
+					
+					
+    @commands.command()
+    async def marisa(self, ctx):
+        char = 'kirisame_marisa'
+        async with aiohttp.ClientSession() as session:
+            async with session.get('http://' + booru + '/index.php?page=dapi&s=post&q=index&tags=solo+' + boorublacklist + '+' + char) as r:
+                if r.status == 200:
+                    soup = BeautifulSoup(await r.text(), "lxml")
+                    num = int(soup.find('posts')['count'])
+                    maxpage = int(round(num/100))
+                    page = random.randint(0, maxpage)
+                    t = soup.find('posts')
+                    p = t.find_all('post')
+                    source = ((soup.find('post'))['source'])
+                    if num < 100:
+                        pic = p[random.randint(0,num-1)]
+                    elif page == maxpage:
+                        pic = p[random.randint(0,num%100 - 1)]
+                    else:
+                        pic = p[random.randint(0,99)]
+                    msg = pic['file_url']
+                    em = discord.Embed(title='', description='Image Source: ' + source, colour=0x42D4F4)
+                    #em.set_author(name='Character Image', icon_url=bot.user.avatar_url)
+                    em.set_author(name='Character Image')
+                    em.set_image(url=booruappend + msg)
+                    await ctx.send(embed=em)
+
+
+    @commands.command()
+    async def tenshi(self, ctx):
+        char = 'hinanawi_tenshi'
+        async with aiohttp.ClientSession() as session:
+            async with session.get('http://' + booru + '/index.php?page=dapi&s=post&q=index&tags=solo+' + boorublacklist + '+' + char) as r:
+                if r.status == 200:
+                    soup = BeautifulSoup(await r.text(), "lxml")
+                    num = int(soup.find('posts')['count'])
+                    maxpage = int(round(num/100))
+                    page = random.randint(0, maxpage)
+                    t = soup.find('posts')
+                    p = t.find_all('post')
+                    source = ((soup.find('post'))['source'])
+                    if num < 100:
+                        pic = p[random.randint(0,num-1)]
+                    elif page == maxpage:
+                        pic = p[random.randint(0,num%100 - 1)]
+                    else:
+                        pic = p[random.randint(0,99)]
+                    msg = pic['file_url']
+                    em = discord.Embed(title='', description='Image Source: ' + source, colour=0x42D4F4)
+                    #em.set_author(name='Character Image', icon_url=bot.user.avatar_url)
+                    em.set_author(name='Character Image')
+                    em.set_image(url=booruappend + msg)
+                    await ctx.send(embed=em)
+
+
+    @commands.command()
+    async def sakuya(self, ctx):
+        char = 'izayoi_sakuya'
+        async with aiohttp.ClientSession() as session:
+            async with session.get('http://' + booru + '/index.php?page=dapi&s=post&q=index&tags=solo+' + boorublacklist + '+' + char) as r:
+                if r.status == 200:
+                    soup = BeautifulSoup(await r.text(), "lxml")
+                    num = int(soup.find('posts')['count'])
+                    maxpage = int(round(num/100))
+                    page = random.randint(0, maxpage)
+                    t = soup.find('posts')
+                    p = t.find_all('post')
+                    source = ((soup.find('post'))['source'])
+                    if num < 100:
+                        pic = p[random.randint(0,num-1)]
+                    elif page == maxpage:
+                        pic = p[random.randint(0,num%100 - 1)]
+                    else:
+                        pic = p[random.randint(0,99)]
+                    msg = pic['file_url']
+                    em = discord.Embed(title='', description='Image Source: ' + source, colour=0x42D4F4)
+                    #em.set_author(name='Character Image', icon_url=bot.user.avatar_url)
+                    em.set_author(name='Character Image')
+                    em.set_image(url=booruappend + msg)
+                    await ctx.send(embed=em)                    
+
+
+                    
 
 def setup(bot):
     bot.add_cog(ImageCog(bot))
