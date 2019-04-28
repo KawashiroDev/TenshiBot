e86a9ef1a8a2061d7aee3ab3f55a4e99ce5337a9
99710 <cuttsharry123@gmail.com>
2019-04-28 12:07:49 +0100

    Updates
    Commands are now case insensitive
    = prefix supported (should help users migrate to mention prefix)
    Error code added but disabled for now

--- Tenshi.py
+++ Tenshi.py
@@ -65,7 +65,12 @@ initial_extensions = ['image']
 
 #bot = commands.Bot(command_prefix= '<@' + str(bot.user.id) + '> ')
 #do not leave '' here
-bot = commands.Bot(command_prefix= tb_prefix)
+
+#ok so with this we can have Tenshi also respond to the = prefix, i'll leave this enabled for a short time then switch to just mention
+#Leave error code commented out until = is disabled because that'll cause chaos
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('='), case_insensitive=True)
+#bot = commands.Bot(command_prefix=commands.when_mentioned, case_insensitive=True)
+#bot = commands.Bot(command_prefix= tb_prefix, case_insensitive=True)
 client = discord.client
 
 if __name__ == '__main__':
@@ -94,8 +99,15 @@ async def on_ready():
     await bot.change_presence(activity=discord.Game(name="Startup complete"))
     await asyncio.sleep(7)
     await bot.change_presence(activity=discord.Streaming(name="TenshiBot", url='https://twitch.tv/99710'))
-    
 
+    
+#error event code
+#print the error to the console and inform the user
+#uncomment when = prefix is removed    
+#@bot.event
+#async def on_command_error(ctx, error):
+#    print(error)
+#    await ctx.send(error)
 
 #other bot ignoring code 
 @bot.event
@@ -112,40 +124,11 @@ async def on_message(message):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-    
 
-
-#Touhou Image commands
-#th_img
-
-#testing this with aiohttp instead of requests, aiohttp is more efficent?
-#This now works            
 @bot.command()
-async def tenshi(ctx):
-    char = 'hinanawi_tenshi'
-    async with aiohttp.ClientSession() as session:
-        async with session.get('http://' + booru + '/index.php?page=dapi&s=post&q=index&tags=solo+' + boorublacklist + '+' + char) as r:
-            if r.status == 200:
-                soup = BeautifulSoup(await r.text(), "lxml")
-                num = int(soup.find('posts')['count'])
-                maxpage = int(round(num/100))
-                page = random.randint(0, maxpage)
-                t = soup.find('posts')
-                p = t.find_all('post')
-                source = ((soup.find('post'))['source'])
-                if num < 100:
-                    pic = p[random.randint(0,num-1)]
-                elif page == maxpage:
-                    pic = p[random.randint(0,num%100 - 1)]
-                else:
-                    pic = p[random.randint(0,99)]
-                msg = pic['file_url']
-                em = discord.Embed(title='', description='Image Source: ' + source, colour=0x42D4F4)
-                em.set_author(name='Character Image', icon_url=bot.user.avatar_url)
-                em.set_image(url=booruappend + msg)
-                await ctx.send(embed=em)            
-
-
+async def errortest(ctx):
+    await()    
+    
 
 #this has to be at the end of the code
 #client.run(token)
