7ed5be3fe5f3c4ccf0446b0b3b54dea0c3719c8e
99710 <cuttsharry123@gmail.com>
2019-04-29 19:26:15 +0100

    Updates
    Added owner checks
    Added some owner commands
    Added some console logging
    Sharding enabled

--- Tenshi.py
+++ Tenshi.py
@@ -21,6 +21,8 @@ bot_version = '1.5'
 #Booting text
 print('Please wait warmly...')
 
+#owner id
+
 import discord
 import requests
 import aiohttp
@@ -28,18 +30,20 @@ import praw
 import lxml
 import random
 import asyncio
+import os
+import subprocess
 
 from discord.ext import commands
 from bs4 import BeautifulSoup
 
-initial_extensions = ['image']
+initial_extensions = ['image', 'booru']
 
 #ok so with this we can have Tenshi also respond to the = prefix, i'll leave this enabled for a short time then switch to just mention
-#Leave error code commented out until = is disabled because that'll cause chaos
-bot = commands.Bot(command_prefix=commands.when_mentioned_or('='), case_insensitive=True)
-#bot = commands.Bot(command_prefix=commands.when_mentioned, case_insensitive=True)
-#bot = commands.Bot(command_prefix= tb_prefix, case_insensitive=True)
-client = discord.client
+#bot = commands.Bot(command_prefix=commands.when_mentioned_or('='), case_insensitive=True)
+bot = commands.AutoShardedBot(command_prefix=commands.when_mentioned, case_insensitive=True)
+
+#Sharding! should help with performance since the bot is on 1000+ servers
+client = discord.AutoShardedClient()
 
 if __name__ == '__main__':
     for extension in initial_extensions:
@@ -58,6 +62,7 @@ async def on_ready():
     print(' ')
     print('User ID - ' + str(bot.user.id))
     print('Username - ' + bot.user.name)
+    print('Shard Count - ' + str(bot.shard_count))
     print('TenshiBot Ver - ' + bot_version)
     print('System Variant - ' + bot_variant)
     print(' ')
@@ -69,12 +74,18 @@ async def on_ready():
 
     
 #error event code
-#print the error to the console and inform the user
-#uncomment when = prefix is removed    
-#@bot.event
-#async def on_command_error(ctx, error):
-#    print(error)
-#    await ctx.send(error)
+#print the error to the console and inform the user   
+@bot.event
+async def on_command_error(ctx, error):
+    #command not failed
+    if isinstance(error, commands.CommandNotFound):
+        return
+    #user failed owner check
+    if isinstance(error, commands.CheckFailure):
+        await ctx.send("not owner")
+    else:
+        print(error)
+        await ctx.send(error)
 
 #other bot ignoring code 
 @bot.event
@@ -86,19 +97,72 @@ async def on_message(message):
         return
     await bot.process_commands(message)
 
+#command logging
+@bot.event
+async def on_command(ctx):
+    print(ctx.message.content)
+    return
+
+#owner check
+def is_owner():
+    async def predicate(ctx):
+        return ctx.author.id == 166189271244472320
+    return commands.check(predicate)
+
+#bot added/kicked from server messages
+@bot.event
+async def on_guild_join(guild):
+        print("[Info] New server get! - " + str(guild))
+        
+@bot.event
+async def on_guild_remove(guild):
+        print("[Info] Kicked from a server - " + str(guild))
+
     
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
 @bot.command()
+@is_owner()
+async def ping2(ctx):
+    await ctx.send('pong')    
+
+@bot.command()
 async def errortest(ctx):
-    await()    
+    await()
+
+
+#basic admin functionality
+@bot.command()
+@is_owner()    
+async def vpsreboot(ctx):
+    #os.system("sudo reboot")
+    os.system("shutdown -r -t 30")
+    await ctx.send('Rebooting the VPS')
+
+#ban test command
+@bot.command()
+@is_owner()
+async def bantest(ctx):
+    await ctx.author.ban(reason=':)')
+    await ctx.author.unban
+
+#console command
+@bot.command()
+@is_owner()
+#freezes the bot!
+async def console(ctx):
+    cmd=ctx.message.content[len("<@571094749537239042> console"):].strip()
+    result = subprocess.check_output([cmd], stderr=subprocess.STDOUT)
+    #os.system(ctx.message.content)
+    await ctx.send(result)    
     
 
 #this has to be at the end of the code
 #client.run(token)
-tkn = open("test/token.txt", "r")
+tkn = open("test/token_debug.txt", "r")
+#tkn = open("test/token_production.txt", "r")
 token = tkn.read()
 tkn.close()    
 bot.run(token, bot=True, reconnect=True)
