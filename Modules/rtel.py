bba64ff038016e663da6090820e62a0bb750a170
99710 <cuttsharry123@gmail.com>
2019-05-02 20:45:17 +0100

    Update rtel.py

--- Modules/rtel.py
+++ Modules/rtel.py
@@ -15,25 +15,25 @@ class rtelCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-@commands.command()
-async def jojo(ctx, arg):
-    await ctx.send(arg + ' has been stopped!', file=discord.File('pics/stop.jpg'))
+    @commands.command()
+    async def jojo(ctx, arg):
+        await ctx.send(arg + ' has been stopped!', file=discord.File('pics/stop.jpg'))
 
-@commands.command()
-async def banana(ctx, arg):
-    await ctx.send(arg + ' has been banaed!', file=discord.File('pics/banana.png'))
+    @commands.command()
+    async def banana(ctx, arg):
+        await ctx.send(arg + ' has been banaed!', file=discord.File('pics/banana.png'))
 
-@commands.command()
-async def oil(ctx, arg):
-    await ctx.send(arg + ' has been oiled!', file=discord.File('pics/oil.png'))
+    @commands.command()
+    async def oil(ctx, arg):
+        await ctx.send(arg + ' has been oiled!', file=discord.File('pics/oil.png'))
 
-@commands.command()
-async def confused(ctx, arg):
-    await ctx.send(file=discord.File('pics/confused.png'))
+    @commands.command()
+    async def confused(ctx, arg):
+        await ctx.send(file=discord.File('pics/confused.png'))
 
-@commands.command()
-async def thonk(ctx, arg):
-    await ctx.send(file=discord.File('pics/thonk.gif'))    
+    @commands.command()
+    async def thonk(ctx, arg):
+        await ctx.send(file=discord.File('pics/thonk.gif'))    
 
 
 					                    
