3d60e0ace4567d995da66a7d5c78201954bdb0f2
99710 <cuttsharry123@gmail.com>
2019-06-17 00:23:50 +0100

    Update debug.py

--- Modules/debug.py
+++ Modules/debug.py
@@ -19,6 +19,22 @@ class debugCog(commands.Cog):
     @commands.command()
     async def d_test(self, ctx):
         await ctx.send('ok!')
+
+        
+#test the booru img embed with various URL's
+    @commands.command()
+    async def imgembed(self, ctx, arg):
+        em = discord.Embed(title='', description='', colour=0x42D4F4)
+        em.set_author(name='imgembed')
+        em.set_image(url=arg)
+        await ctx.send(embed=em)
+
+#test the booru img embed with various URL's
+    @commands.command()
+    async def embedcol(self, ctx, arg):
+        em = discord.Embed(title='テスト', description='テスト', colour=discord.Colour(int(arg)))
+        em.set_author(name='Sample_embed', icon_url=bot.user.avatar_url)
+        await ctx.send(embed=em)
     
 					                    
 
