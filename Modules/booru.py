6e6d032933ba3e64d2936ad6dbaa0f41c8d06302
99710 <cuttsharry123@gmail.com>
2019-12-09 22:50:14 +0000

    Temp safebooru fix
    Changed url for safebooru command to gelbooru and forced safe rating due to a high volume of errors with the command

--- Modules/booru.py
+++ Modules/booru.py
@@ -65,7 +65,8 @@ class booruCog(commands.Cog):
     @commands.cooldown(rlimit_cmd, rlimit_time, commands.BucketType.default)
     async def safebooru(self, ctx, *, tags):
         async with aiohttp.ClientSession() as session:
-            async with session.get('http://' + booru + '/index.php?page=dapi&s=post&q=index&tags=+' + tags) as r:
+            #async with session.get('http://' + booru + '/index.php?page=dapi&s=post&q=index&tags=+' + tags) as r:
+            async with session.get('http://' + 'gelbooru.com' + '/index.php?page=dapi&s=post&q=index&tags=rating:safe+' + tags) as r:
                 if r.status == 200:
                     soup = BeautifulSoup(await r.text(), "lxml")
                     num = int(soup.find('posts')['count'])
